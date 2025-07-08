74a208b6c84459f0800b8d2b76ecd98d66e048fb
Sahibealam2003 <alamsahib9719@gmail.com>
2025-07-08 15:24:54 +0530

    commit

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -1,36 +1,36 @@
-import AddTodo from './AddTodo'
-import ShowTodo from './ShowTodo'
-import Edit from './Edit'
-import TodoSearch from './TodoSearch'
-import { toDosValue } from '../../Utility/TodoContex'
-import { useState } from 'react'
+import AddTodo from "./AddTodo";
+import ShowTodo from "./ShowTodo";
+import Edit from "./Edit";
+import TodoSearch from "./TodoSearch";
+import { toDosValue } from "../../Utility/TodoContex";
+import { useState } from "react";
 
 const Todos = () => {
-  const [searchTodos, setSearchTodos] = useState('')
-  const { isEditing } = toDosValue()
+  const [searchTodos, setSearchTodos] = useState("");
+  const { isEditing } = toDosValue();
   return (
-    <>
-      <div className='border flex justify-center items-center gap-10 w-[100%] min-h-[100vh]  bg-gradient-to-br from-[#2c2c2c] via-[#585858] to-[#bdbdbd] '>
+    <div className="w-[100%] min-h-[100vh]  bg-gradient-to-br from-[#2c2c2c] via-[#585858] to-[#bdbdbd] ">
+      <h1 className="flex justify-center bg-gradient-to-br from-black via-[#23232c] to-[black] bg-clip-text text-transparent text-4xl font-bold mb-5 pt-5">
+        OUR TODOS
+      </h1>
 
-
+      <div className="flex justify-center items-center gap-10 ">
         <div className="drop-shadow-[10px_14px_16px_rgba(0,0,0,0.5)] w-[35%] h-[80vh] bg-[#585858] shadow-2xl">
           {isEditing ? <Edit /> : <AddTodo />}
         </div>
 
-        <div className=' w-[35%]  min-h-[80vh] bg-[#585858] drop-shadow-[10px_14px_16px_rgba(0,0,0,0.5)] shadow-2xl'>
-          
+        <div className=" w-[35%]  min-h-[80vh] bg-[#585858] drop-shadow-[10px_14px_16px_rgba(0,0,0,0.5)] shadow-2xl">
           <TodoSearch setSearchTodos={setSearchTodos} />
           <ShowTodo searchTodos={searchTodos} />
-          
         </div>
-
       </div>
-        <div className='relative'>
-        <h1 className='absolute -top-8 left-[42%] font-medium text-[#585858]'>Made by Sahib e Alam</h1>
+      <div className="relative">
+        <h1 className="absolute top-2 left-[42%] font-medium text-[#23232c]">
+          Made by Sahib e Alam
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    </>
-  )
-}
-
-export default Todos
+export default Todos;
